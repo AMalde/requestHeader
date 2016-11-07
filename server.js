e78d352fd9b3d885a09d79b80fff7967ae33a690
Malde <malde.aleksander@gmail.com>
2016-11-07 14:33:19 +0000

    Second commit

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var useragent = require('useragent');
+var app = express();
+var port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('website'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/api/whoami/', getInfo);
+
+
+function getInfo (req, res) {
+    var ipAddress = (req.headers['x-forwarded-for'] || '').split(',')[0] 
+                    || (req.connection.remoteAddress || '');
+                    
+    var language = req.headers["accept-language"].split(',')[0] || '';
+    var OS = useragent.parse(req.headers['user-agent']).os.family || '';
+    
+    var object = {
+        ipAdress: ipAddress, 
+        language: language,
+        operating_system: OS
+    }
+    
+  res.send(object);
+}
+
+app.listen(port, function () {
+  console.log('listening on port '+ 3000 +'!');
 })

40f703638c886569c945bf03cec69b1d07e95fc1
Malde <malde.aleksander@gmail.com>
2016-11-13 01:34:23 +0000

    last chance

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var useragent = require('useragent');
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.use(express.static('website'));
 
